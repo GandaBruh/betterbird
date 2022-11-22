aa665b89edd7c9a59c6d70cd1e1451c97d27adbe
Ploynapat Bunsena <6310610966@student.tu.ac.th>
2022-11-22 16:49:59 +0700

    final

--- users/admin.py
+++ users/admin.py
@@ -1,17 +1,7 @@
 from django.contrib import admin
-from users.models import LikeBlog, AccountUser, AccountOrganization,Blog,ReportBlog, CookieCoin, Wallet, History, ViewBlog
+from users.models import LikeBlog, AccountUser, AccountOrganization, Blog ,ReportBlog, CookieCoin, Wallet, History, ViewBlog
 # Register your models here.
 admin.site.register([LikeBlog, Blog, AccountUser, AccountOrganization, ReportBlog, CookieCoin, Wallet, History, ViewBlog])
 
-# class ProductAdmin(admin.ModelAdmin):
-#     list_display = ('id', 'name', 'price', 'is_published', 'created_at')
-#     list_display_links = ('id', 'name')
-#     list_filter = ('price',)
-#     list_editable = ('is_published',)
-#     search_fields = ('name', 'price')
-#     ordering = ('price',)
 
-
-# class blogAdmin(admin.ModelAdmin):
-#     list_display = ('id', 'title')
  
